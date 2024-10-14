4cec3df42a257c377ab432e0bbba3294bd78f979
Lawrence N. Wagy <lnwagy@gmail.com>
2024-10-13 21:07:18 -0500

    Add files via upload

---
+++ cbpi4-DS2482S-800-1wireSensor/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='cbpi4-DS2482S-800-1wireSensor',
+      version='0.0.1',
+      description='CraftBeerPi Plugin',
+      author='Lawrence Wagy',
+      author_email='lnwagy@gmail.com',
+      url='https://wagyhof.com',
+      include_package_data=True,
+      package_data={
+        # If any package contains *.txt or *.rst files, include them:
+      '': ['*.txt', '*.rst', '*.yaml'],
+      'cbpi4_DS2482S_800_1wireSensor': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['cbpi4_DS2482S_800_1wireSensor'],
+     )
